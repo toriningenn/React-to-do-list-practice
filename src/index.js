5f0fb56fa75d917cab73e5747cfd2fef5efd312a
toriningenn <annamihailova72@yandex.com>
2021-04-02 11:20:46 +0200

    Done list added

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ class TODOApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tasks: ['kek', 'puk', 'lol'],
-            doneTasks: ['aaa', 'ffff', 'ggg'],
+            tasks: ['kek', 'puk', 'lol', 'lkgl','wtf'],
+            doneTasks: [],
         }
         this.addNewTask = this.addNewTask.bind(this);
         this.deleteThisTask = this.deleteThisTask.bind(this);
-        this.moveTaskToDone = this.moveTaskToDone.bind(this);
+        this.moveTask=this.moveTask.bind(this);
     }
 
     addNewTask(task) {
@@ -28,12 +28,25 @@ class TODOApp extends React.Component {
         }
     }
 
-    moveTaskToDone(taskIndex) {
-        const newArr = [...this.state.doneTasks];
-        const task = this.state.tasks[taskIndex];
-        this.deleteThisTask(taskIndex, this.state.tasks, 'todo')
-        newArr.push(task);
-        this.setState({doneTasks: newArr});
+    moveTask(taskIndex, dest) {
+        let newArr;
+        let task;
+        switch(dest) {
+            case 'toDone':
+                newArr = [...this.state.doneTasks];
+                task = this.state.tasks[taskIndex];
+                this.deleteThisTask(taskIndex, this.state.tasks, 'todo')
+                newArr.push(task);
+                this.setState({doneTasks: newArr});
+                break;
+            case 'backToTodo':
+                newArr = [...this.state.tasks];
+                task = this.state.doneTasks[taskIndex];
+                this.deleteThisTask(taskIndex, this.state.doneTasks,'done')
+                newArr.push(task);
+                this.setState({tasks: newArr});
+                break;
+        }
     }
 
     render() {
@@ -42,7 +55,7 @@ class TODOApp extends React.Component {
                 <Tasks todoList={this.state.tasks}
                        doneList={this.state.doneTasks}
                        deleteFunc={this.deleteThisTask}
-                       moveFunc={this.moveTaskToDone}/>
+                       moveFunc={this.moveTask}/>
                 <MyForm value={this.addNewTask}/>
             </div>
         );
@@ -90,8 +103,10 @@ class Tasks extends React.Component {
                     {this.props.todoList.map(task => (<div>
                         <ul>{task}</ul>
                         <DoneButton
-                            moveToDone={() =>
-                                this.props.moveFunc(this.props.todoList.indexOf(task))}/>
+                            move={() =>
+                                this.props.moveFunc(this.props.todoList.indexOf(task),
+                                    'toDone')}
+                        look={'✓'}/>
                         <DeleteButton
                             value={() => this.props.deleteFunc(
                                 this.props.todoList.indexOf(task),
@@ -104,8 +119,10 @@ class Tasks extends React.Component {
                         {this.props.doneList.map(task => (<div>
                             <ul><s>{task}</s></ul>
                             <DoneButton
-                                moveToDone={() =>
-                                    this.props.moveFunc(this.props.todoList.indexOf(task))}/>
+                                move={() =>
+                                    this.props.moveFunc(this.props.doneList.indexOf(task),
+                                        'backToTodo')}
+                                look={'X'}/>
                             <DeleteButton
                                 value={() => this.props.deleteFunc(
                                     this.props.doneList.indexOf(task),
@@ -123,21 +140,17 @@ class Tasks extends React.Component {
 class DoneButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            done: false,
-        }
         this.clickHandler = this.clickHandler.bind(this);
     }
 
     clickHandler(event) {
         event.preventDefault();
-        this.setState({done: !this.state.done});
-        this.props.moveToDone();
+        this.props.move();
     }
 
     render() {
         return (
-            <button onClick={this.clickHandler}>{this.state.done ? 'X' : '✓'}</button>
+            <button onClick={this.clickHandler}>{this.props.look}</button>
         );
     }
 }
